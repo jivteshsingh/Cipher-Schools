68563c0339643f4438bfd3b673b2d87459d60add
jivtesh singh <pahuls017@gmail.com>
2023-02-24 21:41:50 +0530

    deployement

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,30 @@ const userRoutes = require("./Routes/userRoutes");
 const videoRoutes = require("./Routes/videoRoutes")
 const connectDB = require("./DB");
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+
+// Deployement
+const ___dirname1 = path.resolve();
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(___dirname1,"../frontend/build")));
+
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(___dirname1,"frontend","build","index.html"));
+  })
+}else{
+  app.get("/", (req,res) => {
+      res.send("API is running");
+  });
+}
+
+//
 
 app.use('/api/user',userRoutes);
 app.use('/api/video',videoRoutes);
@@ -19,4 +36,4 @@ app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 4000
 
-const server = app.listen(PORT,console.log(`Server is running on port ${PORT}.`));
+app.listen(PORT,console.log(`Server is running on port ${PORT}.`));
